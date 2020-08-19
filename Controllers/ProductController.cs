73221addbfdd8e6c0789cd169c3103772536beff
Rajani Joshi <68274751+Rajani03Zensar@users.noreply.github.com>
2020-08-19 19:44:45 +0530

    Add models controllers and views of the Product MVC

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using MVCAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAssignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = GetProducts();
+            return View(products);
+        }
+        private IEnumerable<Product> GetProducts() { 
+            return new List<Product>
+           {
+                new Product{ProductId=101,ProductName="AC",Rate=45000},
+                new Product{ProductId=102,ProductName="Mobile",Rate=38000},
+                new Product{ProductId=103,ProductName="Bike",Rate=94000}
+           };
+
+           
+        }
+        public ActionResult Details(int id)
+        {
+            var product = GetProducts().SingleOrDefault(c => c.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+    }
+}
